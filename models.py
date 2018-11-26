fbd64b8c647148a33724b7794957dab290572e0f
satyanarayanamethuku <43133362+satyanarayanamethuku@users.noreply.github.com>
2018-11-26 19:11:08 +0530

    nothing

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from django.db import  models
 # Create your models here.
 class Product(models.Model):
     pid=models.IntegerField(primary_key=True)
-    pname=models.CharField(max_length=20)
+    pname=models.CharField(max_length=30)
     pcost=models.DecimalField(max_digits=10,decimal_places=4)
     pmfd=models.DateField()
     pexpdt=models.DateField()
